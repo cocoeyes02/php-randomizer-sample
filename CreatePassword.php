713a88eed63d358cfb1e1b8aa57383c2e12d14b4
02 <cocoeyes02@gmail.com>
2024-03-30 23:58:15 +0900

    fix(password / fullName / mailAddress): コマンドライン上で実行する前提のコードなので、改行コードを揃えた

--- CreatePassword.php
+++ CreatePassword.php
@@ -3,5 +3,5 @@
 $randomizer = new \Random\Randomizer();
 $canUseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/*-+.,!#$%&()~|_';
 for ($i = 0; $i < 15; $i++) {
-    echo $i + 1 . '個目のパスワード: ' . $randomizer->getBytesFromString($canUseChars, 12) . "<br>";
+    echo $i + 1 . '個目のパスワード: ' . $randomizer->getBytesFromString($canUseChars, 12) . PHP_EOL;
 }
--- CreateTestData.php
+++ CreateTestData.php
@@ -8,12 +8,12 @@ for ($i = 0; $i < 10; $i++) {
         . 'テスト' . $testLastNames[$i]
         . ' '
         . $testFirstNames[$i] . 'テスト'
-        . "<br>";
+        . PHP_EOL;
 }
 
 // 適当な10文字のexample.comメールアドレスを作成する
 $randomizer = new \Random\Randomizer();
 $canUseChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
 for ($i = 0; $i < 15; $i++) {
-    echo $i + 1 . '個目のメールアドレス: ' . $randomizer->getBytesFromString($canUseChars, 10) . '@example.com' . "<br>";
+    echo $i + 1 . '個目のメールアドレス: ' . $randomizer->getBytesFromString($canUseChars, 10) . '@example.com' . PHP_EOL;
 }

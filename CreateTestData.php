829b5504df585eea1ffde4598709a7fb175329c6
02 <cocoeyes02@gmail.com>
2024-03-30 23:54:23 +0900

    feat(mailAddress): 適当な10文字のexample.comメールアドレスを作成するコードを書いた

--- CreateTestData.php
+++ CreateTestData.php
@@ -1,1 +1,7 @@
 <?php
+// 適当な10文字のexample.comメールアドレスを作成する
+$randomizer = new \Random\Randomizer();
+$canUseChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+for ($i = 0; $i < 15; $i++) {
+    echo $i + 1 . '個目のメールアドレス: ' . $randomizer->getBytesFromString($canUseChars, 10) . '@example.com' . "<br>";
+}
